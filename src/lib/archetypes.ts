50d352cd0800e1b3b3a4772909d0fc40857f1037
Louise Ireland <louisesireland@gmail.com>
2026-04-02 16:07:35 -0700

    chore: trigger redeploy

--- src/lib/archetypes.ts
+++ src/lib/archetypes.ts
@@ -1231,3 +1231,4 @@ export function getOpeningThesis(a: string, b: string): string {
   }
   return "Different paths to similar questions. Together, you see more than either would alone.";
 }
+// Force redeploy Thu Apr  2 16:07:35 PDT 2026
